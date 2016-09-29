3009ce7c1318d14b72665b54fc012db6ac62feef
Huw Taylor <huw_taylor@hotmail.co.uk>
2016-09-29 21:50:31 +0100

    added slghtly better drawing of connections and example modules

--- main.lua
+++ main.lua
@@ -15,9 +15,11 @@ timer = 0
 current_step = 0
 
 function love.load()
+
     inputs = {
         Input.new(32, 64,  Input.CLOCK),
         Input.new(32, 128, Input.CLOCK),
+        Input.new(32, 460, Input.CONSTANT)
     }
     gates = {}
     modules = {}
@@ -42,20 +44,49 @@ function love.load()
         delay       = 1
     }
 
-    Input.GROUND = {
-        name = "ground",
-        func =  function (t)
-                    return false
-                end
+    local blinkToConstant = {
+        gates = {
+            {  96, 128, Gate.NOT   },
+            { 192,  96, Gate.OR    },
+        },
+        inputs = {
+            { 1, 1 }, 
+            { 2, 1 }
+        },
+        connections = {
+            -- { gateX, gateX input index, gateY that gateX will use for input },
+            { 2, 2, 1 }
+        },
+        outputs = {
+            2
+        }
     }
 
-    modules[1] = Module.new(64, 16)
-    modules[1]:addGate(Gate.new(96, 128, Gate.NOT, 1))
-    modules[1]:addGate(Gate.new(192, 96, Gate.OR, 2, modules[1].gates[1]))
-    modules[1]:addGate(Gate.new(256, 32, Gate.DELAY, 3))
-    modules[1]:addGate(Gate.new(344, 96, Gate.AND, modules[1].gates[2], modules[1].gates[3]))
+    modules[1] = Module.new(64, 16, blinkToConstant)
+    modules[1]:assignInputs(inputs[2], inputs[1])
 
-    -- modules[1]:assignInputs(inputs[2], inputs[1], inputs[1])
+
+
+    local constantToBlink = {
+        gates = {
+            {  64,  64, Gate.AND   },
+            { 192,  64, Gate.NOT   },
+            { 256,  64, Gate.OR    },
+        },
+        inputs = {
+            { 1, 1 },
+        },
+        connections = {
+            { 1, 2, 2 },
+            { 2, 1, 1 },
+            { 3, 1, 2 },
+            { 3, 2, 2 },
+        },
+        outputs = { 2 }
+    }
+
+    modules[2] = Module.new(64, 344, constantToBlink)
+    modules[2]:assignInputs(inputs[3])
 
 end
 
@@ -90,14 +121,30 @@ function drawGate(gate)
     love.graphics.circle(mode, gate.x + 16, gate.y + 16, 16)
     love.graphics.printf(gate.name, gate.x, gate.y + 36, 32, "center")
     for _, input in pairs(gate.inputs) do
-        if type(input) ~= "number" then
-            love.graphics.line(gate.x, gate.y + 16, input.x + 32, input.y + 16)
-        end
+        drawConnection(input, gate)
     end
 end
 
 function drawModule(mod)
     for _, gate in pairs(mod.gates) do
         drawGate(gate)
+    end
+end
+
+function drawConnection(from, to)
+    if (from.x + 32 > to.x) then
+        local midY
+        if (math.abs(from.y - to.y) < 32) then
+            midY = from.y - 16
+        else
+            midY = (from.y + 16) * 0.7 + (to.y + 16) * 0.3
+        end
+        love.graphics.line(from.x + 32, from.y + 16, from.x + 48, from.y + 16)
+        love.graphics.line(from.x + 48, from.y + 16, from.x + 48, midY)
+        love.graphics.line(from.x + 48, midY, to.x - 16, midY)
+        love.graphics.line(to.x - 16, midY, to.x - 16, to.y + 16)
+        love.graphics.line(to.x, to.y + 16, to.x - 16, to.y + 16)
+    else
+        love.graphics.line(from.x + 32, from.y + 16, to.x, to.y + 16)
     end
 end
